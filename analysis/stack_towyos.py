fb9d6bd93a86eb235b0fc27cf45eb5de47ba2b96
Jesse Cusack <jmcusack@ucsd.edu>
2020-09-17 09:31:13 -0400

    add sig4_sorted to dataset

--- analysis/stack_towyos.py
+++ analysis/stack_towyos.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     datavars = {}
 
     # Fill data dictionary
-    varlist = ["T", "S", "CT", "SA", "u", "v", "sig4", "b", "eps", "LT", "Lo", "N2_overturn"]
+    varlist = ["T", "S", "CT", "SA", "u", "v", "sig4", "sig4_sorted", "b", "eps", "LT", "Lo", "N2_overturn"]
     for var in varlist:
         datavars[var] = (["z", "pfl"], stack2Dvar(TYs_, var, zmin, zmax, dz, npfls))
 
