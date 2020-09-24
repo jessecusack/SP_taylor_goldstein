ee860b2addf0c171841eac2c4878095411e83764
tantanmeow <schontantan@gmail.com>
2020-09-24 23:43:34 +0800

    add new feature: FG tool and second scan

--- sp-tg/codes/FGM.m
+++ sp-tg/codes/FGM.m
@@ -6,11 +6,22 @@
 % HOWTO: 1- linear interp; 2-segment mean
 % BOT: data profile starts from 1-DEEPEST DATA POINT 2- REAL SEA BOTTOM
 % K_init, L_init: initial guesses of wave numbers in x and y directions
-% nu, kap: background viscosity, diffusivity
-% iBC1, iBCN: boundary condition
 % k_thred: theredhold for k ; 1- 2.*pi./7/(bs/2); 2- do not apply
 % bs: bin-size of the measurments (resolution of data)
-% 
+% tool: 1- the Finite difference method (FD) 2- the Fourier-Galerkin method (FG)
+% iBC1, iBCN: boundary condition
+%   FD - 
+%   (1) velocity: 1=rigid (2nd order: w_{hat}=0; 4th order: w_{hat}=0, w_{hat}_z=0), 0=frictionless (2nd order: w_{hat}_z=kw_{hat}; 4th order: w_{hat}=0, w_{hat}_zz=0)
+%   (2) buoyancy: 1=insulating (b_{hat}_z=0 suggesting -\kappa\frac{\pb}{\pz}=0), 0=fixed-buoyancy (b_{hat}=0)                             
+%   FG -
+%   satisfies impermeable, frictionless, constant-buoyancy boundaries (w_{hat}=0,  w_{hat}_zz=0, b_{hat}=0)
+% background viscosity, diffusivity
+%    FD - 
+%    nu, kap (constant or (n,1) columns)
+%    FG - 
+%    Av,Ah,Kv,Kh (constant or (n,1) columns)
+% scan2: 1- only scan once; 2- perform 2nd scan
+%
 % OUTPUTS:
 % v,vz,vzz,b,n2,Ri,zz: profiles used to compute the FGM
 % I_FGM,GR_FGM,CR_FGM,CI_FGM,W_FGM,K_FGM,CL_FGM: FGM
@@ -42,7 +53,7 @@
 %       iii) reduce the vertical grid spacing to 1 m. (new Matlab tool is more efficient)
 % S.Tan, IOCAS, 2019/09/17
 
-function [v,vz,vzz,b,n2,Ri,zz,I_FGM,GR_FGM,CR_FGM,CI_FGM,W_FGM,K_FGM,CL_FGM,II0,GR0,CR0,CI0,W0]=FGM(data,dz,D1,D2,HOWTO,BOT,K_init,L_init,nu,kap,iBC1,iBCN,k_thred,bs)
+function [v,vz,vzz,b,n2,Ri,zz,I_FGM,GR_FGM,CR_FGM,CI_FGM,W_FGM,K_FGM,CL_FGM,II,GR,CR,CI,W]=FGM(data,dz,D1,D2,HOWTO,BOT,K_init,L_init,nu,kap,Av,Ah,Kv,Kh,iBC1,iBCN,k_thred,bs,tool,scan2)
 
     tic
     K = K_init;L = L_init;  
@@ -78,7 +89,21 @@ function [v,vz,vzz,b,n2,Ri,zz,I_FGM,GR_FGM,CR_FGM,CI_FGM,W_FGM,K_FGM,CL_FGM,II0,
              l=find(abs(Ri)<1/4);
              if ~isempty(l)
                  kt=sqrt(K(i)^2+L(j)^2);
-                 [sigs,w]=SSF(zz,v,b,kt,0,nu,kap,iBC1,iBCN,1);
+                 if tool == 1
+                    [sigs,w]=SSF(zz,v,b,kt,0,nu,kap,iBC1,iBCN,1);
+                 elseif tool == 2
+                     n2=BaryL(zz,1,6)*b; % differentiate buoyancy - 6th-order finite difference
+                     vz=BaryL(zz,1,6)*v;vzz=BaryL(zz,2,6)*v;
+                     if length(Av)>1
+                        FG = vTG_FGprep(zz,v,v*0,n2,Av,Ah,Kv,Kh); 
+                        [sigs,w]=vTG_FG(zz,v,v*0,Av,Ah,kt,0,1,FG);
+                     else
+                        FG = vTG_FGprep(zz,v,v*0,n2,ones(size(zz))*Av,ones(size(zz))*Ah,ones(size(zz))*Kv,ones(size(zz))*Kh); 
+                        [sigs,w]=vTG_FG(zz,v,v*0,Av,Ah,kt,0,1,FG);
+                     end
+                 else
+                     error('TOOL MUST BE 1 OR 2')
+                 end
                  inst=real(sigs)/kt;
                  cphs=-imag(sigs)/kt;
                  ww=real(w(:,:));
@@ -93,7 +118,7 @@ function [v,vz,vzz,b,n2,Ri,zz,I_FGM,GR_FGM,CR_FGM,CI_FGM,W_FGM,K_FGM,CL_FGM,II0,
         end
     end
 
-    % ------ seek for FGM ------ 
+    % seek for FGM
     % wavelength therdhold (Hazel, 1972?)
     if k_thred == 2
        [l1, l2]=find(GR0==max(max(GR0)));
@@ -103,22 +128,108 @@ function [v,vz,vzz,b,n2,Ri,zz,I_FGM,GR_FGM,CR_FGM,CI_FGM,W_FGM,K_FGM,CL_FGM,II0,
     else
        error('k_thred must be 1 or 2');
     end
-    
-    % ------- second scan ------%  
-    %                           %
-    %       to be added soon    %
-    %                           %
-    % --------------------------%  
-
-
     II=II0;GR=GR0;CR=CR0;CI=CI0;W=W0;
+
     if ~isempty(l1)
-        I_FGM = II(l1,l2);
-        GR_FGM = GR(l1,l2);
-        CR_FGM = CR(l1,l2);
-        CI_FGM = CI(l1,l2);
-        W_FGM = W(:,l1,l2);
-        K_FGM = [K(l1) L(l2)];
+    if scan2==2
+    % ------- second scan ------%  
+    % 1. reduced grid spacing (1/4) in the k, l plane
+        if length(K)==1
+            K_iter = K;
+        else
+            K_iter = 10.^(log10(K(l1))-log10(K(2)/K(1))*3:log10(K(2)/K(1))/4:log10(K(l1))+log10(K(2)/K(1))*3);
+        end
+        if length(L)==1
+            L_iter = L;
+        else
+            L_iter = 10.^(log10(L(l2))-log10(L(2)/L(1))*3:log10(L(2)/L(1))/4:log10(L(l2))+log10(L(2)/L(1))*3);
+        end            
+    % 2. reduce the vertical grid spacing to dz/2
+        % data processing step-0
+        [v,vz,vzz,b,n2,Ri,zz]=DP(data,dz/2,D1,D2,K_iter(1),L_iter(1),HOWTO,BOT);
+        % data processing step-1 (optional)
+    %     [v,vz,vzz,b,n2,Ri,zz]=DP_1(v,b,zz); 
+        % data processing step-2
+    %     [v,vz,vzz,b,n2,Ri,zz]=DP_2(v,b,n2,zz,dz/2,100); % low pass filter :(... ,dz/2,fl)
+        % data processing step-3
+    %     [v,vz,vzz,b,n2,Ri,zz]=DP_3(v,b,n2,zz,dz/2);
+        [v,vz,vzz,b,n2,Ri,zz]=DP_0(v,vz,vzz,b,n2,Ri,zz);
+        KT1=nan(length(K_iter),length(L_iter));
+        II1=nan(length(K_iter),length(L_iter));
+        GR1=nan(length(K_iter),length(L_iter));
+        CR1=nan(length(K_iter),length(L_iter));
+        CI1=nan(length(K_iter),length(L_iter));
+        W1=nan(length(zz),length(K_iter),length(L_iter));
+
+        for i=1:length(K_iter)
+            for j=1:length(L_iter)           
+                [v,vz,vzz,b,n2,Ri,zz]=DP(data,dz/2,D1,D2,K_iter(i),L_iter(j),HOWTO,BOT);
+                % data processing step-1 (optional)
+            %     [v,vz,vzz,b,n2,Ri,zz]=DP_1(v,b,zz); 
+                % data processing step-2
+            %     [v,vz,vzz,b,n2,Ri,zz]=DP_2(v,b,n2,zz,dz/2,100); % low pass filter :(... ,dz/2,fl)
+                % data processing step-3
+            %     [v,vz,vzz,b,n2,Ri,zz]=DP_3(v,b,n2,zz,dz/2);
+                [v,vz,vzz,b,n2,Ri,zz]=DP_0(v,vz,vzz,b,n2,Ri,zz); 
+
+                 % skip for min(Ri_r)>1/4 
+                 l=find(abs(Ri)<1/4);
+                 if ~isempty(l)
+                     kt=sqrt(K_iter(i)^2+L_iter(j)^2);
+                     if tool == 1
+                        [sigs,w]=SSF(zz,v,b,kt,0,nu,kap,iBC1,iBCN,1);
+                     elseif tool == 2
+                         n2=BaryL(zz,1,6)*b; % differentiate buoyancy - 6th-order finite difference
+                         vz=BaryL(zz,1,6)*v;vzz=BaryL(zz,2,6)*v;
+                         if length(Av)>1
+                            FG = vTG_FGprep(zz,v,v*0,n2,Av,Ah,Kv,Kh); 
+                            [sigs,w]=vTG_FG(zz,v,v*0,Av,Ah,kt,0,1,FG);
+                         else
+                            FG = vTG_FGprep(zz,v,v*0,n2,ones(size(zz))*Av,ones(size(zz))*Ah,ones(size(zz))*Kv,ones(size(zz))*Kh); 
+                            [sigs,w]=vTG_FG(zz,v,v*0,Av,Ah,kt,0,1,FG);
+                         end
+                     else
+                         error('TOOL MUST BE 1 OR 2')
+                     end
+                     inst=real(sigs)/kt;
+                     cphs=-imag(sigs)/kt;
+                     ww=real(w(:,:));
+                     I=turning_pt(ww,0); % turing point          
+                     II1(i,j)=I(1);
+                     GR1(i,j)=real(sigs(1));
+                     CR1(i,j)=cphs(1);
+                     CI1(i,j)=inst(1);
+                     W1(:,i,j)=w;
+                     KT1(i,j)=kt;
+                 end
+            end
+        end
+    % 3. estimate the location of the GR maximum using a biquadratic fit 
+        if k_thred == 2
+           [l1, l2]=find(GR1==max(max(GR1)));
+        elseif k_thred == 1
+            a=GR1; a(find(KT1>2*pi/7/(bs/2)))=0;
+            [l1, l2]=find(GR1==max(max(a)));       
+        else
+           error('k_thred must be 1 or 2');
+        end
+        K = K_iter;L = L_iter;  
+        II=II1;GR=GR1;CR=CR1;CI=CI1;W=W1;
+        [v,vz,vzz,b,n2,Ri,zz]=DP(data,dz/2,D1,D2,K(l1),L(l2),HOWTO,BOT);
+        % data processing step-1 (optional)
+    %     [v,vz,vzz,b,n2,Ri,zz]=DP_1(v,b,zz); 
+        % data processing step-2
+    %     [v,vz,vzz,b,n2,Ri,zz]=DP_2(v,b,n2,zz,dz/2,100); % low pass filter :(... ,dz/2,fl)
+        % data processing step-3
+    %     [v,vz,vzz,b,n2,Ri,zz]=DP_3(v,b,n2,zz,dz/2);
+        [v,vz,vzz,b,n2,Ri,zz]=DP_0(v,vz,vzz,b,n2,Ri,zz);   
+         if tool == 2
+             n2=BaryL(zz,1,6)*b; % differentiate buoyancy - 6th-order finite difference
+             vz=BaryL(zz,1,6)*v;vzz=BaryL(zz,2,6)*v;
+         end
+    elseif scan2==1
+        K = K_init;L = L_init;  
+        II=II0;GR=GR0;CR=CR0;CI=CI0;W=W0; 
         [v,vz,vzz,b,n2,Ri,zz]=DP(data,dz,D1,D2,K(l1),L(l2),HOWTO,BOT);
         % data processing step-1 (optional)
     %     [v,vz,vzz,b,n2,Ri,zz]=DP_1(v,b,zz); 
@@ -127,7 +238,21 @@ function [v,vz,vzz,b,n2,Ri,zz,I_FGM,GR_FGM,CR_FGM,CI_FGM,W_FGM,K_FGM,CL_FGM,II0,
         % data processing step-3
     %     [v,vz,vzz,b,n2,Ri,zz]=DP_3(v,b,n2,zz,dz);
         [v,vz,vzz,b,n2,Ri,zz]=DP_0(v,vz,vzz,b,n2,Ri,zz);   
-        
+         if tool == 2
+             n2=BaryL(zz,1,6)*b; % differentiate buoyancy - 6th-order finite difference
+             vz=BaryL(zz,1,6)*v;vzz=BaryL(zz,2,6)*v;
+         end
+    else
+        error('SCAN2 MUST BE 1 OR 2')
+    end
+    
+        I_FGM = II(l1,l2);
+        GR_FGM = GR(l1,l2);
+        CR_FGM = CR(l1,l2);
+        CI_FGM = CI(l1,l2);
+        W_FGM = W(:,l1,l2);
+        K_FGM = [K(l1) L(l2)];
+
         % critical level
         [tp,l]=turning_pt(v,CR_FGM);CL_FGM=[];
         if tp == 0
